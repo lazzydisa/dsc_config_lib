786433bbdcd7d273ca2dd95e451841ca9b6cc186
lazzydisa <dimasalinov07@gmail.com>
2026-08-16 19:45:26 +0300

    adding all files...

---
+++ src/config.rs
@@ -0,0 +1,79 @@
+/* dsc config protocol */
+
+// struct that contains variable's name and its value
+pub struct Var {
+    pub name:  String,
+    pub value: String,
+}
+
+impl Var {
+    pub fn get_name(&self) -> String {
+        self.name.clone()
+    }
+
+    pub fn get_value(&self) -> String {
+        self.value.clone()
+    }
+
+    pub fn parse_line(line: &str) -> Self {
+        let (mut name, mut value) = (String::new(), String::new());
+
+        // getting variable's name
+        for var in line.chars() {
+            match var {
+                '"' | '\'' => continue, // don't use that characters in names please
+                ' ' | '\t' => continue,
+                '#' | ':'  => break,
+                _ => name.push(var)
+            }
+        }
+
+        // getting variable's value
+        let (mut test1, mut test2, mut c_test) = (false, false, 'x');
+        for val in line.chars() {
+            if val != ':' && test1 == false { continue; }
+
+            test1 = true;
+
+            match val {
+                ':'  => continue,
+                '"'  => if c_test != '\'' {
+                    if test2 == false {
+                        test2 = true;
+                        c_test = '"';
+                        continue;
+                    } else {
+                        break;
+                    }
+                } else { value.push(val); },
+                '\'' => if c_test != '"' {
+                    if test2 == false { 
+                        test2 = true;
+                        c_test = '\'';
+                        continue;
+                    } else {
+                        break;
+                    }
+                } else { value.push(val); },
+                ' '  => if test2 == false { continue; } else { value.push(val); },
+                '#'  => if test2 == false { break; } else { value.push(val); },
+                _    => value.push(val),
+            }
+        }
+
+        Self {
+            name,
+            value,
+        }
+    }
+
+    pub fn collect_to_vec(st: &str) -> Vec<Self> {
+        let mut v: Vec<Self> = Vec::new();
+
+        for i in st.lines() {
+            v.push(Self::parse_line(i));
+        }
+
+        v
+    }
+}
---
+++ test.dsc
@@ -0,0 +1,14 @@
+# comment
+sas: "i dont know what to say"
+asas: "aaaaaaaaaa" # xd
+
+# tests
+test1: value
+test2:value_again # how about without spaces?
+test3: sas asas # will not work correctly
+test4: "test of '#' "
+    test5: ?
+	test6: "test for tabulation"
+test7:      "sasI"
+"test8": dddddd
+'test9': 'test of the "#"'
